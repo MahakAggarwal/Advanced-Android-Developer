c1acf25ce50362334dfea50bcb08284cf53b46b2
Mahak Aggarwal <39831824+MahakAggarwal@users.noreply.github.com>
2019-08-19 21:55:52 +0530

    Add files via upload

---
+++ 7.1 Device location/WalkMyAndroidCC/app/src/main/java/me/mahakagg/walkmyandroidcc/FetchAddressTask.java
@@ -0,0 +1,75 @@
+package me.mahakagg.walkmyandroidcc;
+
+import android.content.Context;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.os.AsyncTask;
+import android.text.TextUtils;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+// this class is used for reverse geo-coding
+public class FetchAddressTask extends AsyncTask <Location, Void, String> {
+    private OnTaskCompleted onTaskCompleted;
+    private Context mContext;
+
+    public FetchAddressTask(Context context, OnTaskCompleted taskCompleted) {
+        this.mContext = context;
+        this.onTaskCompleted = taskCompleted;
+    }
+
+    @Override
+    protected String doInBackground(Location... locations) {
+        Geocoder geocoder = new Geocoder(mContext, Locale.getDefault());
+        // get location from the passed in parameter
+        Location location = locations[0];
+        // list of addresses needed for geo-coder
+        List<Address> addresses = null;
+        // empty string to hold final address
+        String resultMessage = "";
+
+        // reverse geo-coding
+        try{
+            // get one address result from geo-coder
+            addresses = geocoder.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
+            // if no addresses are found, set result message
+            if (addresses == null || addresses.size() == 0){
+                if (resultMessage.isEmpty()){
+                    resultMessage = mContext.getString(R.string.no_address_found);
+                }
+            }
+            else{
+                // if address is found, read the address into resultMessage string
+                Address address = addresses.get(0);
+                ArrayList<String> addressParts = new ArrayList<>();
+                // get address lines and put them in array list
+                for (int i = 0; i <= address.getMaxAddressLineIndex(); i++){
+                    addressParts.add(address.getAddressLine(i));
+                }
+                // concatenate array list contents into a string
+                resultMessage = TextUtils.join("\n", addressParts);
+            }
+        }
+        catch (IOException e){
+            resultMessage = mContext.getString(R.string.service_not_available);
+        }
+        catch (IllegalArgumentException e){
+            resultMessage = mContext.getString(R.string.invalid_lat_long_used);
+        }
+        return resultMessage;
+    }
+
+    @Override
+    protected void onPostExecute(String s) {
+        onTaskCompleted.onTaskCompleted(s);
+        super.onPostExecute(s);
+    }
+
+    interface OnTaskCompleted{
+        void onTaskCompleted(String result);
+    }
+}
---
+++ 7.1 Device location/WalkMyAndroidHW/app/src/main/java/me/mahakagg/walkmyandroid/MainActivity.java
@@ -0,0 +1,182 @@
+package me.mahakagg.walkmyandroid;
+
+import android.Manifest;
+import android.animation.AnimatorInflater;
+import android.animation.AnimatorSet;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+
+/*
+* Answers to homework questions
+*
+* Q1 - Which API do you use to request the last known location on the device?
+* A1 - getLastLocation() method in the FusedLocationProviderClient class
+*
+* Q2 - Which class do you use for handling geocoding and reverse geocoding?
+* A2 - Geocoder
+*
+* Q3 - Which method do you use for periodic location updates ?
+* A3 - requestLocationUpdates() method in the FusedLocationProviderClient class
+*
+* */
+public class MainActivity extends AppCompatActivity implements FetchAddressTask.OnTaskCompleted {
+    private static final int REQUEST_LOCATION_PERMISSION = 1;
+    private static final String TRACKING_LOCATION_KEY = "tracking_location_key";
+    //    private Location mLastLocation;
+    private TextView mLocationTextView;
+    private Button mButton;
+    private FusedLocationProviderClient mFusedLocationProviderClient;
+    private ImageView mAndroidImageView;
+    private AnimatorSet mRotateAnim;
+    private boolean mTrackingLocation;
+    private LocationCallback mLocationCallback;
+    private Location currentLocation;
+    private TextView mSpeedAccuracyTextView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // find views
+        mButton = findViewById(R.id.button_location);
+        mLocationTextView = findViewById(R.id.textview_location);
+        mAndroidImageView = findViewById(R.id.imageview_android);
+        mSpeedAccuracyTextView = findViewById(R.id.speed_accuracy_textView);
+        // animator setup
+        mRotateAnim = (AnimatorSet) AnimatorInflater.loadAnimator(this, R.animator.rotate);
+        mRotateAnim.setTarget(mAndroidImageView);
+        // set on click listener for button
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!mTrackingLocation) {
+                    startTrackingLocation();
+                } else {
+                    stopTrackingLocation();
+                }
+            }
+        });
+        // restore savedInstanceState if possible
+        if (savedInstanceState != null) {
+            mTrackingLocation = savedInstanceState.getBoolean(TRACKING_LOCATION_KEY);
+        }
+        // initialize fusedLocationProviderClient object
+        mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+        // fusedLocationProviderClient invokes the onLocationResult() method when location update is requested
+        mLocationCallback = new LocationCallback() {
+            @Override
+            public void onLocationResult(LocationResult locationResult) {
+                // if tracking is on, start reverse geo-coding
+                if (mTrackingLocation) {
+                    currentLocation = locationResult.getLastLocation();
+                    new FetchAddressTask(MainActivity.this, MainActivity.this).execute(currentLocation);
+                }
+            }
+        };
+    }
+
+    private void startTrackingLocation() {
+        // check for permission
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // if not granted, ask for permission
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_LOCATION_PERMISSION);
+        } else {
+            // permission already granted
+            // request periodic location updates
+            mFusedLocationProviderClient.requestLocationUpdates(getLocationRequest(), mLocationCallback, null);
+        }
+        // show loading text while asyncTask runs
+        mLocationTextView.setText(getString(R.string.address_text, getString(R.string.loading), System.currentTimeMillis()));
+        // start animation, change boolean value, and change button text
+        mRotateAnim.start();
+        mTrackingLocation = true;
+        mButton.setText(R.string.stop_tracking_location);
+    }
+
+    // this method executes when ActivityCompat requests permission from user
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == REQUEST_LOCATION_PERMISSION) {
+            // if permission granted, get location
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                startTrackingLocation();
+            } else {
+                // if permission not granted, show toast
+                Toast.makeText(this, getString(R.string.location_permission_denied), Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private void stopTrackingLocation() {
+        if (mTrackingLocation) {
+            mTrackingLocation = false;
+            mButton.setText(R.string.get_location);
+            mLocationTextView.setText(R.string.textview_hint);
+            mRotateAnim.end();
+            mFusedLocationProviderClient.removeLocationUpdates(mLocationCallback);
+            mSpeedAccuracyTextView.setText("");
+        }
+    }
+
+    // LocationRequest object has setter methods which determine frequency and accuracy of location updates
+    private LocationRequest getLocationRequest() {
+        LocationRequest locationRequest = new LocationRequest();
+        locationRequest.setInterval(10000)
+                .setFastestInterval(5000)
+                .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        return locationRequest;
+    }
+
+    // update textView with result from AsyncTask's interface
+    @Override
+    public void onTaskCompleted(String result) {
+        if (mTrackingLocation) {
+            mLocationTextView.setText(getString(R.string.address_text, result, System.currentTimeMillis()));
+            // get speed and accuracy of location
+            float speed, accuracy;
+            speed = currentLocation.getSpeed();
+            accuracy = currentLocation.getAccuracy();
+            mSpeedAccuracyTextView.setText(getString(R.string.speed_accuracy_text, speed, accuracy));
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (mTrackingLocation){
+            startTrackingLocation();
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (mTrackingLocation){
+            stopTrackingLocation();
+            mTrackingLocation = true;
+        }
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        outState.putBoolean(TRACKING_LOCATION_KEY, mTrackingLocation);
+        super.onSaveInstanceState(outState);
+    }
+}
