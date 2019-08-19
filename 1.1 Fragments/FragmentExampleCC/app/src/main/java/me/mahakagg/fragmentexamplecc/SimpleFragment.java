60317efbf8f489f14cd7feec9a96bfc44f301c94
Mahak Aggarwal <39831824+MahakAggarwal@users.noreply.github.com>
2019-08-19 21:31:36 +0530

    Add files via upload

---
+++ 1.1 Fragments/FragmentExampleCC/app/src/main/java/me/mahakagg/fragmentexamplecc/SimpleFragment.java
@@ -0,0 +1,68 @@
+package me.mahakagg.fragmentexamplecc;
+
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RadioGroup;
+import android.widget.RatingBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class SimpleFragment extends Fragment {
+    private static final int YES = 0;
+    private static final int NO = 1;
+
+    // used for when android needs to re-instantiate a fragment when needed
+    public SimpleFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        // setup of root view (layout) and radio group
+        final View rootView = inflater.inflate(R.layout.fragment_simple, container, false);
+        final RadioGroup group = rootView.findViewById(R.id.radio_group);
+        final RatingBar ratingBar = rootView.findViewById(R.id.ratingBar);
+
+        // check change listener for radio group
+        group.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                View radioButton = group.findViewById(checkedId);
+                int index = group.indexOfChild(radioButton);
+                TextView textView = rootView.findViewById(R.id.fragment_header);
+                switch (index) {
+                    case YES:
+                        textView.setText(R.string.yes_message);
+                        break;
+                    case NO:
+                        textView.setText(R.string.no_message);
+                        break;
+                    default: break;
+                }
+            }
+        });
+
+        ratingBar.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
+            @Override
+            public void onRatingChanged(RatingBar ratingBar, float rating, boolean fromUser) {
+                String myRating = (getString(R.string.my_rating) + ratingBar.getRating());
+                Toast.makeText(getContext(), myRating, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        return rootView;
+    }
+
+}
