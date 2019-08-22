5bc7baf69cc4f9457f4508875760b751e77d5adf
Mahak Aggarwal <39831824+MahakAggarwal@users.noreply.github.com>
2019-08-22 18:00:58 +0530

    Create MainActivity.java

---
+++ 8.1 Places API/WalkMyAndroidPlacesCC/app/src/main/java/me/mahakagg/walkmyandroidplacescc/MainActivity.java
@@ -0,0 +1,311 @@
+package me.mahakagg.walkmyandroidplacescc;
+
+import android.Manifest;
+import android.animation.AnimatorInflater;
+import android.animation.AnimatorSet;
+import android.annotation.SuppressLint;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.libraries.places.api.Places;
+import com.google.android.libraries.places.api.model.Place;
+import com.google.android.libraries.places.api.model.PlaceLikelihood;
+import com.google.android.libraries.places.api.net.FindCurrentPlaceRequest;
+import com.google.android.libraries.places.api.net.FindCurrentPlaceResponse;
+import com.google.android.libraries.places.api.net.PlacesClient;
+import com.google.android.libraries.places.widget.AutocompleteSupportFragment;
+import com.google.android.libraries.places.widget.listener.PlaceSelectionListener;
+
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+
+/*
+ * Task 3 - Place Picker is deprecated so this task is not valid here
+ * */
+/*
+ * For information about the new places SDK visit - https://developers.google.com/places/android-sdk/intro
+ * */
+public class MainActivity extends AppCompatActivity implements FetchAddressTask.OnTaskCompleted {
+
+    // Constants
+    private static final int REQUEST_LOCATION_PERMISSION = 1;
+    private static final String TRACKING_LOCATION_KEY = "tracking_location";
+    // Views
+    private Button mLocationButton;
+    private TextView mLocationTextView;
+    private ImageView mAndroidImageView;
+    // Location classes
+    private boolean mTrackingLocation;
+    private FusedLocationProviderClient mFusedLocationClient;
+    private LocationCallback mLocationCallback;
+    private static final String TAG = MainActivity.class.getSimpleName();
+    // Animation
+    private AnimatorSet mRotateAnim;
+
+    private PlacesClient placesClient;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mLocationButton = findViewById(R.id.button_location);
+        mLocationTextView = findViewById(R.id.textview_location);
+        mAndroidImageView = findViewById(R.id.imageview_android);
+        // Initialize the FusedLocationClient.
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+        // Set up the animation.
+        mRotateAnim = (AnimatorSet) AnimatorInflater.loadAnimator(this, R.animator.rotate);
+        mRotateAnim.setTarget(mAndroidImageView);
+        // Restore the state if the activity is recreated.
+        if (savedInstanceState != null) {
+            mTrackingLocation = savedInstanceState.getBoolean(TRACKING_LOCATION_KEY);
+        }
+        // Set the listener for the location button.
+        mLocationButton.setOnClickListener(new View.OnClickListener() {
+            /**
+             * Toggle the tracking state.
+             * @param v The track location button.
+             */
+            @Override
+            public void onClick(View v) {
+                if (!mTrackingLocation) {
+                    startTrackingLocation();
+                } else {
+                    stopTrackingLocation();
+                }
+            }
+        });
+        // Initialize the location callbacks.
+        mLocationCallback = new LocationCallback() {
+            /**
+             * This is the callback that is triggered when the
+             * FusedLocationClient updates your location.
+             * @param locationResult The result containing the device location.
+             */
+            @Override
+            public void onLocationResult(LocationResult locationResult) {
+                // If tracking is turned on, reverse geocode into an address
+                if (mTrackingLocation) {
+                    new FetchAddressTask(MainActivity.this, MainActivity.this).execute(locationResult.getLastLocation());
+                }
+            }
+        };
+        // initialize places
+        Places.initialize(getApplicationContext(), getResources().getString(R.string.google_places_key));
+        // create a new Places client instance
+        placesClient = Places.createClient(this);
+
+        // AUTOCOMPLETE IMPLEMENTATION
+        // initialize autocomplete fragment
+        final AutocompleteSupportFragment autocompleteSupportFragment = (AutocompleteSupportFragment) getSupportFragmentManager().findFragmentById(R.id.autocomplete_fragment);
+        // specifies types of place data to return
+        if (autocompleteSupportFragment != null) {
+            autocompleteSupportFragment.setPlaceFields(Arrays.asList(Place.Field.NAME, Place.Field.ADDRESS, Place.Field.TYPES));
+            // setup placeSelectionListener to handle the response
+            autocompleteSupportFragment.setOnPlaceSelectedListener(new PlaceSelectionListener() {
+                @Override
+                public void onPlaceSelected(@NonNull Place place) {
+                    // change drawable icon
+                    setAndroidType(place);
+                    // modify textView
+                    mLocationTextView = autocompleteSupportFragment.getView().findViewById(R.id.textview_location);
+                    mLocationTextView.setText(getString(R.string.address_text, place.getName(), place.getAddress(), System.currentTimeMillis()));
+                }
+
+                @Override
+                public void onError(@NonNull Status status) {
+                    mLocationTextView.setText(R.string.no_place);
+                }
+            });
+        }
+    }
+
+    /**
+     * Starts tracking the device. Checks for
+     * permissions, and requests them if they aren't present. If they are,
+     * requests periodic location updates, sets a loading text and starts the
+     * animation.
+     */
+    private void startTrackingLocation() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_LOCATION_PERMISSION);
+        } else {
+            mTrackingLocation = true;
+            mFusedLocationClient.requestLocationUpdates(getLocationRequest(), mLocationCallback, null);
+
+            // Set a loading text while you wait for the address to be
+            // returned
+            mLocationTextView.setText(getString(R.string.address_text, getString(R.string.loading), getString(R.string.loading), new Date()));
+            mLocationButton.setText(R.string.stop_tracking_location);
+            mRotateAnim.start();
+        }
+    }
+
+
+    /**
+     * Stops tracking the device. Removes the location
+     * updates, stops the animation, and resets the UI.
+     */
+    private void stopTrackingLocation() {
+        if (mTrackingLocation) {
+            mTrackingLocation = false;
+            mLocationButton.setText(R.string.start_tracking_location);
+            mLocationTextView.setText(R.string.textview_hint);
+            mRotateAnim.end();
+        }
+    }
+
+
+    /**
+     * Sets up the location request.
+     *
+     * @return The LocationRequest object containing the desired parameters.
+     */
+    private LocationRequest getLocationRequest() {
+        LocationRequest locationRequest = new LocationRequest();
+        locationRequest.setInterval(10000);
+        locationRequest.setFastestInterval(5000);
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        return locationRequest;
+    }
+
+
+    /**
+     * Saves the last location on configuration change
+     */
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        outState.putBoolean(TRACKING_LOCATION_KEY, mTrackingLocation);
+        super.onSaveInstanceState(outState);
+    }
+
+    /**
+     * Callback that is invoked when the user responds to the permissions
+     * dialog.
+     *
+     * @param requestCode  Request code representing the permission request
+     *                     issued by the app.
+     * @param permissions  An array that contains the permissions that were
+     *                     requested.
+     * @param grantResults An array with the results of the request for each
+     *                     permission requested.
+     */
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == REQUEST_LOCATION_PERMISSION) {// If the permission is granted, get the location, otherwise,
+            // show a Toast
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                startTrackingLocation();
+            } else {
+                Toast.makeText(this, R.string.location_permission_denied, Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    @Override
+    public void onTaskCompleted(final String result) {
+        if (mTrackingLocation) {
+            // Update the UI
+//            mLocationTextView.setText(getString(R.string.address_text, result, result, System.currentTimeMillis()));
+
+            //use fields to define data types to return
+            List<Place.Field> placeFields = Arrays.asList(Place.Field.NAME, Place.Field.ADDRESS, Place.Field.TYPES);
+            // Use the builder to create a FindCurrentPlaceRequest.
+            FindCurrentPlaceRequest request = FindCurrentPlaceRequest.builder(placeFields).build();
+
+            // setup onCompleteListener
+            placesClient.findCurrentPlace(request).addOnCompleteListener(new OnCompleteListener<FindCurrentPlaceResponse>() {
+                @Override
+                public void onComplete(@NonNull Task<FindCurrentPlaceResponse> task) {
+                    Place currentPlace = null;
+                    if (task.isSuccessful()) {
+                        FindCurrentPlaceResponse likelyPlaces = task.getResult();
+                        double maxLikelihood = 0;
+                        if (likelyPlaces != null) {
+                            for (PlaceLikelihood placeLikelihood : likelyPlaces.getPlaceLikelihoods()) {
+                                if (maxLikelihood < placeLikelihood.getLikelihood()) {
+                                    maxLikelihood = placeLikelihood.getLikelihood();
+                                    currentPlace = placeLikelihood.getPlace();
+                                }
+                            }
+                        }
+                        // change textView text
+                        if (currentPlace != null) {
+                            mLocationTextView.setText(getString(R.string.address_text, currentPlace.getName(), result, System.currentTimeMillis()));
+                            setAndroidType(currentPlace);
+                        }
+                    } else {
+                        mLocationTextView.setText(getString(R.string.address_text, "No place name found!", result, System.currentTimeMillis()));
+                    }
+                }
+            });
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        if (mTrackingLocation) {
+            stopTrackingLocation();
+            mTrackingLocation = true;
+        }
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        if (mTrackingLocation) {
+            startTrackingLocation();
+        }
+        super.onResume();
+    }
+
+    // change image of android robot based on type of currentPlace
+    private void setAndroidType(Place currentPlace) {
+        int drawableId = -1;
+        List<Place.Type> placeType = currentPlace.getTypes();
+
+        if (placeType != null) {
+            for (Place.Type typeOfPlace : placeType) {
+                switch (typeOfPlace) {
+                    case SCHOOL:
+                        drawableId = R.drawable.android_school;
+                        break;
+                    case GYM:
+                        drawableId = R.drawable.android_gym;
+                        break;
+                    case RESTAURANT:
+                        drawableId = R.drawable.android_restaurant;
+                        break;
+                    case LIBRARY:
+                        drawableId = R.drawable.android_library;
+                        break;
+                }
+            }
+        }
+        if (drawableId < 0) {
+            drawableId = R.drawable.android_plain;
+        }
+        mAndroidImageView.setImageResource(drawableId);
+    }
+
+}
